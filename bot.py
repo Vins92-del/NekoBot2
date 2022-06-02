de9758265dad37439975b06a76e8634654782b43
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-02 20:20:44 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ class Bot(Client):
         except Exception as a:
             self.LOGGER(__name__).warning(a)
             self.LOGGER(__name__).info(
-                "Bot Berhenti. Gabung Group https://t.me/bling2indonesia untuk Bantuan"
+                "Bot Berhenti. Gabung Group https://t.me/fsubneko untuk Bantuan"
             )
             sys.exit()
 
@@ -59,7 +59,7 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Gabung Group https://t.me/bling2indonesia untuk Bantuan"
+                    "Bot Berhenti. Gabung Group https://t.me/fsubneko untuk Bantuan"
                 )
                 sys.exit()
 
@@ -79,7 +79,7 @@ class Bot(Client):
                     f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "Bot Berhenti. Gabung Group https://t.me/bling2indonesia untuk Bantuan"
+                    "Bot Berhenti. Gabung Group https://t.me/fsubneko untuk Bantuan"
                 )
                 sys.exit()
 
@@ -94,13 +94,13 @@ class Bot(Client):
                 f"Pastikan @{self.username} adalah admin di Channel DataBase anda, CHANNEL_ID Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "Bot Berhenti. Gabung Group https://t.me/bling2indonesia untuk Bantuan"
+                "Bot Berhenti. Gabung Group https://t.me/fsubneko untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/bling2indonesia"
+            f"[🤖 I'am Alive!]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan\nSilahkan Tanyakan di Grup https://t.me/fsubneko"
         )
 
     async def stop(self, *args):

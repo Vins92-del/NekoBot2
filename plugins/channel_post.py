a0d27200429cf34ae934b63f45f159b996049eb1
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-02 09:40:53 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -11,7 +11,7 @@ from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start", "users", "broadcast", "ping", "uptime", "batch", "genlink']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...!", quote = True)
+    reply_text = await message.reply_text("<code>Tunggu Sebentar...!</code>", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:

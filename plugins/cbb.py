d3a68885799b2313ca30a9aaf02addfcc171d7b1
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-02 16:58:07 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,7 +14,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=f"<b>Tentang Bot ini:\n\n@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus\n\n × Creator: @{OWNER}\n × Source Code: <a href='https://github.com/BTRExo/File-Sharing-Neko'>File-Sharing-Neko</a>\n × Owner Repo: @Nekocannn\n\n👮‍♂️ Develoved by @Nekocannn</b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
+                [[InlineKeyboardButton("- ᴛᴜᴛᴜᴘ -", callback_data="close")]]
             ),
         )
     elif data == "close":

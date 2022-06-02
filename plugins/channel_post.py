58aca5f7ffb32c7a4f074b55c1730d6f536ce46f
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-02 15:10:59 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz
+# (©)Codexbotz
 
 import asyncio
 
@@ -19,7 +19,7 @@ from helper_func import encode
     )
 )
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("<code>Tunggu Sebentar...!</code>", quote=True)
+    reply_text = await message.reply_text("<code>Tunggu Sebentar...</code>", quote=True)
     try:
         post_message = await message.copy(
             chat_id=client.db_channel.id, disable_notification=True
@@ -31,7 +31,7 @@ async def channel_post(client: Client, message: Message):
         )
     except Exception as e:
         print(e)
-        await reply_text.edit_text("<b>Telah Terjadi Error...!</b>")
+        await reply_text.edit_text("<b>Telah Terjadi Error...</b>")
         return
     converted_id = post_message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
